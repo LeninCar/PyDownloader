65ef452035d0ea580edb2c22f43b7aef26243b99
LeninCar <lenin.carabali@correounivalle.edu.co>
2024-05-01 20:33:19 -0500

    Second Commit

--- parcial_1.py
+++ parcial_1.py
@@ -1,10 +1,11 @@
 import subprocess
 import os
 
-def descargar_video(url, directorio_destino):
+def descargar_video(url, directorio_destino, nombre_personalizado):
     if not os.path.exists(directorio_destino):
         os.makedirs(directorio_destino)
-    comando_descarga = ['yt-dlp', '-o', f'{directorio_destino}/%(title)s.%(ext)s', url]
+    # Reemplazar guiones con espacios en blanco en el nombre personalizado
+    comando_descarga = ['yt-dlp', '-o', f'{directorio_destino}/{nombre_personalizado}', url]
     subprocess.run(comando_descarga)
 
 def extraer_audio(titulo_video, video_path, directorio_destino):
@@ -14,20 +15,28 @@ def extraer_audio(titulo_video, video_path, directorio_destino):
     subprocess.run(comando_extraccion)
     os.remove(video_path)
 
-# URL del nuevo vídeo de YouTube a descargar
-url_video = "https://www.youtube.com/watch?v=sOS9aOIXPEk"
+if __name__ == '__main__':
+    # URL del nuevo vídeo de YouTube a descargar
+    url_video = "https://www.youtube.com/watch?v=tdwWhVCrQhU"
 
-# Directorio donde se guardarán los archivos descargados
-directorio_destino = "/home/lecm/Videos"
+    # Directorio donde se guardarán los archivos descargados
+    directorio_destino = "/home/lecm/Videos"
 
-# Descargar el vídeo
-descargar_video(url_video, directorio_destino)
+    comando_info_video = ['yt-dlp', '-e', url_video]
+    comando_info_video_ext = ['yt-dlp', '--get-filename', '-o', '%(ext)s', url_video]
+    # comando_info_video_ext = ['yt-dlp', '-e', '--get-filename', '-o', '%(ext)s', url_video]
+    titulo_video = subprocess.check_output(comando_info_video, text=True).strip()
+    titulo_video_ext = subprocess.check_output(comando_info_video_ext, text=True).strip()
+    titulo_video = titulo_video.replace("-", "")
 
-comando_info_video = ['yt-dlp', '-e', url_video]
-titulo_video = subprocess.check_output(comando_info_video, text=True).strip()
+    # Descargar el vídeo
+    descargar_video(url_video, directorio_destino,titulo_video)
 
-# Obtener la ruta del vídeo descargado
-ruta_video = f'{directorio_destino}/{titulo_video}.webm'
+    # Obtener la ruta del vídeo descargado
+    ruta_video = f'{directorio_destino}/{titulo_video}.{titulo_video_ext}'
 
-# Extraer el audio del vídeo descargado
-extraer_audio(titulo_video, ruta_video, directorio_destino)
+    # print("----------------------------------------", ruta_video)
+
+
+    # Extraer el audio del vídeo descargado
+    extraer_audio(titulo_video, ruta_video, directorio_destino)
